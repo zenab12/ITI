944f7974ef65f08efd8fb8de1a7fe6fa13654397
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2022-12-03 18:19:46 +0200

    Add files via upload

---
+++ D05/demo/par.js
@@ -0,0 +1,10 @@
+// var para = document.getElementById("PAR");
+// function ChangeFont(f){
+// para.style.fontFamily  = f;
+
+// }
+
+// function ChangeAlign(v){
+//     para.style.textAlign = v;
+
+// }
