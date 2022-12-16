cc349606f360eaad9dfaf4e1a536668a92431bd1
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2022-12-16 18:26:24 +0200

    Update script.js

--- HTML5/media_player/script.js
+++ HTML5/media_player/script.js
@@ -1,4 +1,4 @@
-$(function (evt) {
+$(function(evt) {
 
     var off_onBtn = $(".btn.off_on");
     var startBtn = $(".start_pause")
@@ -9,7 +9,7 @@ $(function (evt) {
     var audio = $("audio");
     var audioLength = $("input.audio-length");
     var play = $(".play");
-    var title= $(".description h2");
+    var title = $(".description h2");
     var songImg = $(".songs .active li img");
     var favImg = $(".fav img");
 
@@ -25,27 +25,26 @@ $(function (evt) {
     var isPlaying = false;
     var updateTimer;
     var curr_track = document.querySelector("audio");
-    let track_list = [
-        {
+    let track_list = [{
             name: "night",
             artist: "omar",
             image: "./imgs/disc_1.png",
             path: "./sounds/1.mp3",
-            poster:"./imgs/kid-1077793_640.jpg"
+            poster: "./imgs/kid-1077793_640.jpg"
         },
         {
             name: "walking",
             artist: "Abdelaziz",
             image: "./imgs/disc_2.png",
             path: "./sounds/2.mp3",
-            poster:"./imgs/arab-618749_640.jpg"
+            poster: "./imgs/arab-618749_640.jpg"
         },
         {
             name: "back",
             artist: "Abdullah",
             image: "./imgs/disc_3.png",
             path: "./sounds/3.mp3",
-            poster:"./imgs/man-6527045_640.jpg"
+            poster: "./imgs/man-6527045_640.jpg"
 
         },
     ];
@@ -61,7 +60,7 @@ $(function (evt) {
         curr_track.src = track_list[track_index].path;
         $(disc).find($("img")).attr("src", track_list[track_index].image)
         curr_track.load();
-        
+
         $(title).text(track_list[track_index].name);
         // Set an interval of 1000 milliseconds
         // for updating the seek slider
@@ -72,10 +71,10 @@ $(function (evt) {
         curr_track.addEventListener("ended", nextTrack);
 
     }
-  
 
-    $(songImg).on("click",function(e){
-        $(songImg).each(function(ind,el){
+
+    $(songImg).on("click", function(e) {
+        $(songImg).each(function(ind, el) {
             el.dataset.index = ind;
 
             track_index = (e.target).dataset.index;
@@ -83,10 +82,9 @@ $(function (evt) {
         });
 
         curr_track.src = track_list[track_index].path;
-        $(disc).find($("img")).attr("src", track_list[track_index].image
-        );
+        $(disc).find($("img")).attr("src", track_list[track_index].image);
         curr_track.load();
-        
+
         $(title).text(track_list[track_index].name);
         // Set an interval of 1000 milliseconds
         // for updating the seek slider
@@ -99,7 +97,7 @@ $(function (evt) {
     });
 
 
-    $(favImg).each(function(ind,el){
+    $(favImg).each(function(ind, el) {
         el.dataset.index = ind;
         track_index = (el).dataset.index;
         curr_track.src = track_list[track_index].path;
@@ -109,8 +107,8 @@ $(function (evt) {
 
     //function to resest song based of index
 
-    $(favImg).on("click",function(e){
-        $(favImg).each(function(ind,el){
+    $(favImg).on("click", function(e) {
+        $(favImg).each(function(ind, el) {
             el.dataset.index = ind;
             track_index = (e.target).dataset.index;
             curr_track.src = track_list[track_index].path;
@@ -118,7 +116,7 @@ $(function (evt) {
         });
 
         curr_track.load();
-        
+
         $(title).text(track_list[track_index].name);
         // Set an interval of 1000 milliseconds
         // for updating the seek slider
@@ -130,13 +128,13 @@ $(function (evt) {
         playTrack();
     });
 
-  
+
     // Function to reset all values to their default
     function resetValues() {
         curr_time.textContent = "00:00";
         total_duration.textContent = "00:00";
         seek_slider.value = 0;
-        ($(audioLength)[0]).value=0;
+        ($(audioLength)[0]).value = 0;
     }
 
 
@@ -148,7 +146,8 @@ $(function (evt) {
         else pauseTrack();
     }
     playpause_btn.onclick = playpauseTrack;
-    console.log( $($(startBtn)[0]))
+    console.log($($(startBtn)[0]))
+
     function playTrack() {
         // Play the loaded track
 
@@ -218,12 +217,13 @@ $(function (evt) {
     }
 
     function seekToDisc() {
-        seekto = curr_track.duration * (  ($(audioLength)[0]).value / 100);
+        seekto = curr_track.duration * (($(audioLength)[0]).value / 100);
         curr_track.currentTime = seekto;
     }
 
     seek_slider.onchange = seekTo;
-    ($(audioLength)[0]).onchange=seekToDisc;
+    ($(audioLength)[0]).onchange = seekToDisc;
+
     function seekUpdate() {
         let seekPosition = 0;
 
@@ -240,10 +240,18 @@ $(function (evt) {
             let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
             // Add a zero to the single digit time values
-            if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
-            if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-            if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
-            if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+            if (currentSeconds < 10) {
+                currentSeconds = "0" + currentSeconds;
+            }
+            if (durationSeconds < 10) {
+                durationSeconds = "0" + durationSeconds;
+            }
+            if (currentMinutes < 10) {
+                currentMinutes = "0" + currentMinutes;
+            }
+            if (durationMinutes < 10) {
+                durationMinutes = "0" + durationMinutes;
+            }
 
             // Display the updated duration
             curr_time.textContent = currentMinutes + ":" + currentSeconds;
@@ -252,66 +260,103 @@ $(function (evt) {
     }
 
     //check if selected area is button as image big so mush and change cursor to pointer if area true
-   
-    $($(off_onBtn).find($(".circle_btn"))[0]).mousemove(function (e) {
-        
-            $($(off_onBtn)[0]).on("click", "div.circle_btn", (function (e) {
-                if ($($(off_onBtn).find($("img"))[0]).attr("src") == "./imgs/onButton.png") {
-                    $($(off_onBtn).find($("img"))[0]).attr("src", "./imgs/offButton.png");
-                    $(disc).find($("img")).css({ "top": "-300px" });
-                    $("body").css({"background-color":"#fff","color":"#222"});
-                    $(".menu li i").css({"color":"#222"});
-                    $("aside a").css({"color":"#222"});
-                    $("aside p").css({"color":"#222"});
-                    $("header img").attr("src","./imgs/robot.png");
-                    $("header h1").css({"color":"#222"});
-                    $("body").addClass("light");
-                    $("body").removeClass("dark");
 
-                } else {
-                    $($(off_onBtn).find($("img"))[0]).attr("src", "./imgs/onButton.png");
-                    $(disc).find($("img")).css({ "top": "0px" });
-                    $("body").css({"background-color":"#222","color":"#fff"});
-                    $(".menu li i").css({"color":"#fff"});
-                    $("aside a").css({"color":"#fff"});
-                    $("aside p").css({"color":"#fff"});
-                    $("header img").attr("src","./imgs/robot(1).png");
-                    $("header h1").css({"color":"#fff"});
-                    $("body").addClass("dark");
-                    $("body").removeClass("light");
+    var flag = 1;
+    $($(off_onBtn)[0]).on("click", "div.circle_btn", (function(e) {
+        if (flag == 1) {
+            if ($($(off_onBtn).find($("img"))[0]).attr("src") == "./imgs/onButton.png") {
+                $($(off_onBtn).find($("img"))[0]).attr("src", "./imgs/offButton.png");
+                $(disc).find($("img")).css({
+                    "top": "-300px"
+                });
+                $("body").css({
+                    "background-color": "#fff",
+                    "color": "#222"
+                });
+                $(".menu li i").css({
+                    "color": "#222"
+                });
+                $("aside a").css({
+                    "color": "#222"
+                });
+                $("aside p").css({
+                    "color": "#222"
+                });
+                $("header img").attr("src", "./imgs/robot.png");
+                $("header h1").css({
+                    "color": "#222"
+                });
+                $("body").addClass("light");
+                $("body").removeClass("dark");
+
+            } else {
+                $($(off_onBtn).find($("img"))[0]).attr("src", "./imgs/onButton.png");
+                $(disc).find($("img")).css({
+                    "top": "0px"
+                });
+                $("body").css({
+                    "background-color": "#222",
+                    "color": "#fff"
+                });
+                $(".menu li i").css({
+                    "color": "#fff"
+                });
+                $("aside a").css({
+                    "color": "#fff"
+                });
+                $("aside p").css({
+                    "color": "#fff"
+                });
+                $("header img").attr("src", "./imgs/robot(1).png");
+                $("header h1").css({
+                    "color": "#fff"
+                });
+                $("body").addClass("dark");
+                $("body").removeClass("light");
 
 
-                }
-                $($(off_onBtn).find($("img"))[0]).toggleClass("active");
-            }));
-    });
+            }
+        } else {
+            flag -= 1;
+        }
+        console.log(flag);
+        $($(off_onBtn).find($("img"))[0]).toggleClass("active");
+    }));
 
 
 
-    $(startBtn).find($(".circle_btn")).mousemove(function (e) {
-            $($(startBtn)[0]).on("click", ".circle_btn", (function (e) {
+    $(startBtn).find($(".circle_btn")).mousemove(function(e) {
+        $($(startBtn)[0]).on("click", ".circle_btn", (function(e) {
+            if (flag == 1) {
                 playpauseTrack();
 
-            }));
+            } else {
+                flag -= 1;
+            }
+
+        }));
     });
 
 
-    $($($(controller)[0]).find($("div.circle_btn"))[0]).mousemove(function (e) {
-            $(this).on("click", (function (e) {
-                playpauseTrack();
-            }));
-       
-    });
+    $($($(controller)[0]).find($("div.circle_btn"))[0]).on("click", (function(e) {
+        if (flag == 1) {
+            playpauseTrack();
+
+        } else {
+            flag -= 1;
+        }
+    }));
+
 
 
     $(volumeCtrl).val(Math.round((($(audio)[0]).volume) * 10));
-    $(volumeCtrl).on("input", function () {
+    $(volumeCtrl).on("input", function() {
         ($(audio)[0]).volume = parseInt(($(this).val())) * 0.1;
     });
 
 
-    
-    ($(audio)[0]).onseeked = function () {
+
+    ($(audio)[0]).onseeked = function() {
 
         if (($(audio)[0]).duration > 0 && !(($(audio)[0]).paused)) {
             $(play).find($("img.ctrlbtn")).attr("src", "./imgs/pause-button.png");
@@ -319,12 +364,12 @@ $(function (evt) {
             $(play).find($("img.ctrlbtn")).attr("src", "./imgs/play-button.png");
         }
 
-        ($(audio)[0]).onplaying = function () {
+        ($(audio)[0]).onplaying = function() {
             $(play).find($("img.ctrlbtn")).attr("src", "./imgs/pause-button.png");
             $(disc).find($("img")).addClass("active");
         };
 
-        ($(audio)[0]).onended = function () {
+        ($(audio)[0]).onended = function() {
             $(play).find($("img.ctrlbtn")).attr("src", "./imgs/play-button.png");
             $(disc).find($("img")).removeClass("active");
         };
@@ -333,7 +378,7 @@ $(function (evt) {
     };
 
 
-    ($(audio)[0]).onvolumechange = function () {
+    ($(audio)[0]).onvolumechange = function() {
         $(volumeCtrl).val(Math.round((($(this)[0]).volume) * 10));
         if (($(audio)[0]).volume == 0) {
             $(volume).find($("img.ctrlbtn")).attr("src", "./imgs/volume-mute.png");
@@ -347,73 +392,73 @@ $(function (evt) {
     loadTrack(track_index);
 
     var tab = $(".tab");
-    $(tab).click(function (e) {
-        $($("." + $(this).attr("id"))[0]).on("click",function(e){
-        $($(this).find($("img"))).each(function(ind,el){
+    $(tab).click(function(e) {
+        $($("." + $(this).attr("id"))[0]).on("click", function(e) {
+            $($(this).find($("img"))).each(function(ind, el) {
                 el.dataset.index = ind;
-                $(el).on("click",function(e){
-                    track_index =(e.target).dataset.index;
+                $(el).on("click", function(e) {
+                    track_index = (e.target).dataset.index;
                     console.log(e.target);
                     curr_track.src = track_list[track_index].path;
                     $(disc).find($("img")).attr("src", track_list[track_index].image);
                 })
             })
 
-                curr_track.load();
-                
-                $(title).text(track_list[track_index].name);
-                // Set an interval of 1000 milliseconds
-                // for updating the seek slider
-                updateTimer = setInterval(seekUpdate, 1000);
-        
-                // Move to the next track if the current finishes playing
-                // using the 'ended' event
-                curr_track.addEventListener("ended", nextTrack);
-                playTrack();
-            });
+            curr_track.load();
+
+            $(title).text(track_list[track_index].name);
+            // Set an interval of 1000 milliseconds
+            // for updating the seek slider
+            updateTimer = setInterval(seekUpdate, 1000);
+
+            // Move to the next track if the current finishes playing
+            // using the 'ended' event
+            curr_track.addEventListener("ended", nextTrack);
+            playTrack();
+        });
         $(e.target).toggleClass("active");
         $(e.target).siblings().removeClass("active");
         $($("." + $(e.target).attr("id"))[0]).toggleClass("active");
         $($("." + $(this).attr("id"))[0]).siblings().removeClass("active");
         console.log($("body").hasClass("light"))
-    
 
 
-        });
-      
-       
+
     });
 
 
+});
 
-    //old bad code 
-    // $(startBtn).find($(".circle_btn")).mousemove(function (e) {
-    //     // if ((e.clientX >= 500 && e.clientX < 560 && e.clientY < 375 && e.clientY >= 320)) {
-    //         $($(startBtn)[0]).on("click", ".circle_btn", (function (e) {
-    //             // ($(audio)[0]).ontimeupdate = function () {
-    //             //     $(audioLength).val(($(audio)[0]).currentTime);
-    //             // };               
-    //             // if (($(audio)[0]).duration > 0 && !(($(audio)[0]).paused)) {
 
-    //             //     ($(audio)[0]).pause();
-    //             //     $(play).find($("img.ctrlbtn")).attr("src", "./imgs/play-button.png");
-    //             //     pauseTrack();
 
-    //             // } else {
-    //             //     ($(audio)[0]).play();
-    //             //     playTrack();
-    //             //     $(play).find($("img.ctrlbtn")).attr("src", "./imgs/pause-button.png");
+//old bad code 
+// $(startBtn).find($(".circle_btn")).mousemove(function (e) {
+//     // if ((e.clientX >= 500 && e.clientX < 560 && e.clientY < 375 && e.clientY >= 320)) {
+//         $($(startBtn)[0]).on("click", ".circle_btn", (function (e) {
+//             // ($(audio)[0]).ontimeupdate = function () {
+//             //     $(audioLength).val(($(audio)[0]).currentTime);
+//             // };               
+//             // if (($(audio)[0]).duration > 0 && !(($(audio)[0]).paused)) {
 
-    //             // }
+//             //     ($(audio)[0]).pause();
+//             //     $(play).find($("img.ctrlbtn")).attr("src", "./imgs/play-button.png");
+//             //     pauseTrack();
 
-    //             playpauseTrack();
+//             // } else {
+//             //     ($(audio)[0]).play();
+//             //     playTrack();
+//             //     $(play).find($("img.ctrlbtn")).attr("src", "./imgs/pause-button.png");
 
-    //         }));
-    // });
-    // // $(audioLength).attr("max", parseInt(($(audio)[0]).duration));
-    // // $(audioLength).val(($(audio)[0]).currentTime);
+//             // }
 
-    // // $(audioLength).on("input", function () {
-    // //     curr_track.currentTime = $(this).val();
-    // //     console.log($(this).val());
-    // // })
+//             playpauseTrack();
+
+//         }));
+// });
+// // $(audioLength).attr("max", parseInt(($(audio)[0]).duration));
+// // $(audioLength).val(($(audio)[0]).currentTime);
+
+// // $(audioLength).on("input", function () {
+// //     curr_track.currentTime = $(this).val();
+// //     console.log($(this).val());
+// // })
