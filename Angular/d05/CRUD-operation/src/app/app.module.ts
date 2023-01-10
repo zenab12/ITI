0b785f39bbdf13b6a43ad2f402e19f3e9716610c
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-01-10 15:59:34 +0200

    update

---
+++ Angular/d05/CRUD-operation/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UpdateComponent } from './components/update/update.component';
+import { RegisterComponent } from './components/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UpdateComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
