d47c4c9b85f54eb774017336ec6f13230f355420
zenab12 <zienmo555@outlook.com>
2023-01-30 01:07:47 +0200

    finish lab 3
    finshed lab 3 but it is not complete as clean code and the code miss important rules and conditions to make sematic res messages and error messages

--- Nodejs/ds_3/index.js
+++ Nodejs/ds_3/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const { readFile, writeFile } = require("fs").promises;
 const app = express();
+const path = require("path");
+const morgan = require("morgan");
 const port = 3000;
 
 app.use(express.json());
+app.use(express.static());
+app.use(morgan("dev"));
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).res.send("internal server error");
+  }
+  next();
+});
 app.post("/register", (req, res) => {
   // console.log(req.body[0]);
   let data = req.body;
@@ -12,9 +22,7 @@ app.post("/register", (req, res) => {
   data.forEach((element) => {
     if (element.username && element.firstName && element.password) {
       (async () => {
-        await writeFile("db.json", JSON.stringify(req.body, null, 4), {
-          flag: "a",
-        });
+        await writeFile("db.json", JSON.stringify(req.body, null, 4));
       })();
       res.send("{message:”user was registered successfully”}");
     } else if (
@@ -35,8 +43,6 @@ app.post("/register", (req, res) => {
       element.password == ""
     ) {
       attr = "password";
-    } else {
-      attr = "123";
     }
     res.send(`{error:{${attr}} is required`);
   });
@@ -55,13 +61,19 @@ app.post("/login", (req, res) => {
         let login = db.filter((ele) => {
           if (ele.username === element.username) {
             console.log("loggedin");
-            return true;
+            ele.login = true;
+            return ele;
           } else {
-            return false;
+            ele.login = false;
+            return ele;
           }
         });
-
+        (async () => {
+          await writeFile("db.json", JSON.stringify(login, null, 4));
+        })();
         if (login.length > 0) {
+          element.login = true;
+          console.log(element);
           res.send({
             message: "logged in successfully",
             profile: { name: element.username, firstName: element.firstName },
@@ -87,6 +99,91 @@ app.post("/login", (req, res) => {
       res.send(`{error:{${attr}} is required`);
     }
   });
+});
+
+app.get("/todos", (req, res, next) => {
+  (async () => {
+    let page = await readFile("todos.json", "utf-8");
+    res.end(page);
+  })();
+});
+
+app.post("/todos", (req, res, next) => {
+  (async () => {
+    let dbStr = await readFile("db.json", "utf-8");
+    let db = JSON.parse(dbStr);
+    console.log(req.body.username);
+
+    let loggedEle = db.filter((ele) => {
+      if (ele.login && ele.username === req.body.username) {
+        (async () => {
+          const ctrStr = await readFile("counter.json", "utf-8");
+          const ctr = JSON.parse(ctrStr);
+          ctr.counter++;
+          ele.title = req.body.title;
+          ele.status = req.body.status ? req.body.status : "todo";
+          (ele.id = ctr.counter),
+            await writeFile("counter.json", JSON.stringify(ctr, null, 4));
+        })();
+        return `{"username":${ele.username},"title":${ele.title}}`;
+      }
+    });
+
+    if (loggedEle.length > 0) {
+      let todostr = await readFile("todos.json", "utf-8");
+      let todos = JSON.parse(todostr);
+      console.log(
+        `{"username":${loggedEle[0].username},"title":${loggedEle[0].title}}`
+      );
+      todos.push({
+        username: loggedEle[0].username,
+        title: loggedEle[0].title,
+        status: loggedEle[0].status,
+        id: loggedEle[0].id,
+      });
+
+      await writeFile("todos.json", JSON.stringify(todos, null, 4));
+      res.end("todo created successfully");
+    } else {
+      res.end(
+        "error happened check you are loggedin again please or your username is exist"
+      );
+    }
+  })();
+});
+
+app.delete("/todos/:id", (req, res, next) => {
+  const { id } = req.params;
+  (async () => {
+    let taskstr = await readFile("todos.json", "utf-8");
+    let tasks = JSON.parse(taskstr);
+    console.log(tasks);
+    let notRemovedTask = tasks.filter((task) => {
+      return +id !== task.id;
+    });
+    await writeFile("todos.json", JSON.stringify(notRemovedTask, null, 4));
+    res.end("successfully removed");
+  })();
+});
+
+app.patch("/todos/:id", (req, res, next) => {
+  const { id } = req.params;
+  (async () => {
+    let taskstr = await readFile("todos.json", "utf-8");
+    let tasks = JSON.parse(taskstr);
+    const { title, status } = req.body;
+    let editTask = tasks.map((task) => {
+      return +id === task.id
+        ? {
+            ...task,
+            title,
+            status,
+          }
+        : task;
+    });
+    res.send("updating suscess");
+    await writeFile("todos.json", JSON.stringify(editTask, null, 4));
+  })();
 });
 
 app.listen(port, () => {
