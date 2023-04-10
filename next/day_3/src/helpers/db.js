ed0c4ae8c650ffc50cfe5dd6289bd24476eb8a8d
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-04-10 21:11:39 +0200

    Update db.js

--- next/day_3/src/helpers/db.js
+++ next/day_3/src/helpers/db.js
@@ -1,9 +1,21 @@
 
 import { MongoClient } from "mongodb"
+const uri = 'mongodb+srv://Zien:Zien1234@atlascluster.aokq6ht.mongodb.net/?retryWrites=true&w=majority';
+
 const dbConnection =async ()=>{
-    let dbConnect = await MongoClient.connect(process.env.uri,{ useNewUrlParser: true, useUnifiedTopology: true });
-   console.log(dbConnect.db())
-    return dbConnect.db();
+    const client =new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+      
+   const res = await client.connect();
+  return res.db("ecomerce");
+
+    // let dbConnect = await MongoClient.connect(process.env.uri,{ useNewUrlParser: true, useUnifiedTopology: true });
+    // return dbConnect;
 }
+
+
+
 
 export default dbConnection
