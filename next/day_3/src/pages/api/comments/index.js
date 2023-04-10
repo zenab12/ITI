4b27e61eb5f0a1fcb8f7046def0b1e4c23d893e5
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-04-10 15:50:39 +0200

    Update index.js

--- next/day_3/src/pages/api/comments/index.js
+++ next/day_3/src/pages/api/comments/index.js
@@ -7,9 +7,15 @@ export default async function handler(req, res) {
         // res.json(data);
         if (req.method == 'POST')
         {
-           const {username,email,content,product_id,date} = req.body;
-           const comment =  await collection.insertOne({username,email,content,product_id,date});
-           res.status(200).json({message:comment});
+         try {
+            console.log(process.env.uri);
+            const {username,email,content,product_id,date} = req.body;
+            const comment =  await collection.insertOne({username,email,content,product_id,date});
+            res.status(200).json({message:comment});
+            } catch (error) {
+            // Send error response
+            res.status(500).json({ success: false, message: 'Error creating comment', error });
+          }
         }
         res.status(200).json({ message: 'welcome',data})
   }
