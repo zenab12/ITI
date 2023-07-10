437a6c120ef5a9782b7b5c8f4ed95a21f7a8b45a
zenab12 <zenabm@std.mans.edu.eg>
2023-07-10 16:46:02 +0300

    edit assignments files

---
+++ javascript/D03&D04/task.js
@@ -0,0 +1,49 @@
+// function dateValid(strDate){
+//     //dd-mm-year;
+//     if(strDate.length!==10||strDate[2]!=="-"||strDate[5]!=="-"){
+//         throw("string length 10")
+//     }
+//     //console.log(strDate[3]+strDate[4]);
+//     var splitArray = strDate.split("-");
+//     //console.log(splitArray);//[dd-mm-year]
+//     var year =splitArray[2] ;
+//     var month = splitArray[1];
+//     var day =splitArray[0] ;
+//     // console.log(year);
+//     // console.log(month);
+//     // console.log(day);
+//     return new Date(year,month-1,day)
+
+
+// }
+
+// // var d = dateValid("24-11-2022");
+// try{
+
+//     var res  = prompt("enter date formate DD-MM-YYYY");
+//     var d = dateValid(res);
+//     document.write(d);
+// }catch{
+//     alert("wrong format")
+// }
+
+/////////////////////////////////////
+
+
+//console.log(x);
+
+//24-11-2022
+//11-24-2022
+//day 24 mon 11 year 2022
+// try{
+    
+
+// }
+// catch{
+//     alert("try again")
+
+// }
+
+
+
+// alert("hello");
